676eec85bc906ce07a77cfdff2eb1f2c74590c99
jSohnData <jihos@me.com>
2023-05-08 23:14:42 -0400

    FIXED bug with gh-pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
 import Homepage from './pages/Homepage.jsx';
 import Form from './pages/Form.jsx';
 import Portfolio from './pages/Portfolio.jsx';
 import About from './pages/About.jsx';
-
 import './styles/App.css';
 
 function App() {
@@ -12,7 +10,7 @@ function App() {
     <BrowserRouter> 
       <Routes>
         <Route path='/' element={ <Homepage />} />
-        <Route path='/form/:id?' element={ <Form />} />
+        <Route path='/form/:docid?' element={ <Form />} />
         <Route path='/portfolio' element={ <Portfolio />} />
         <Route path='/about' element={ <About />} />
       </Routes>
